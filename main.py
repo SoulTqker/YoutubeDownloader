8a2cbeac20d0a878e7e9e94cc26b50b34fb4a98b
SoulTqker <69645213+SoulTqker@users.noreply.github.com>
2020-11-28 20:44:50 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+import pytube
+import os
+from time import sleep
+
+username = os.getlogin()
+video_list = []
+
+print("Welcome aboard the trash Youtube-Downloader of the Wati-Dev")
+print("Loading... (its long cause im bad)")
+sleep(3)
+
+print('''
+Wat you want?
+
+(1) Download Vids Manually
+(2) Downloads YouTube Playlist
+
+''')
+
+
+
+
+
+def res():
+    resolution = quality
+    if resolution in ["low", "360", "360p"]:
+        itag = 18
+    elif resolution in ["medium", "720", "720p", "hd"]:
+        itag = 22
+    else:
+        itag = 18
+    return itag
+
+
+def path():
+    global chemin
+    p = c
+    if p.lower() in ['downloads', 'download']:
+        chemin = f'C:/Users/{username}/Downloads'
+    elif p.lower() in ['desktop']:
+        chemin = f'C:/Users/{username}/Desktop'
+    elif p.lower() in ['documents, document']:
+        chemin = f'C:/Users/{username}/Documents'
+    elif p.lower() in ['videos', 'video']:
+        chemin = f'C:/Users/{username}/Videos'
+
+
+    return chemin
+
+def videos():
+    print('Enter the url(s) (End by "STOP") :')
+    while True:
+        url = input("")
+        if url.lower() == 'stop':
+            break
+        video_list.append(url)
+
+    for x, video in enumerate(video_list):
+        v = pytube.YouTube(video)
+        stream = v.streams.get_by_itag(res())
+        print(f'Downloading the video number {x}...')
+        stream.download(output_path=path())
+        print(f'Finished for the number {x}')
+    return print("Done my brudda")
+
+def playlist():
+
+    print("Enter the Playlist url :")
+    url = input("")
+    playlist = pytube.Playlist(url)
+    for url in playlist:
+        video = pytube.YouTube(url)
+        stream = video.streams.get_by_itag(22)
+        stream.download(output_path=path())
+    return print("It's gud dude")
+
+
+choice = input("Bad choice: ")
+
+if choice == "1" or choice == "2":
+    quality = input("Choose your resolution (360p, 720p): ")
+    c = input("Choose your downloading folder : Downloads, Desktop, Documents, Videos :")
+    if choice == "1":
+        videos()
+    if choice == "2":
+        playlist()
+
+
